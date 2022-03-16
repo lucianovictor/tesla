d854641da58a608bc10f9485efe9acb490d86678
Luciano <54756923+lucianovictor@users.noreply.github.com>
2022-03-16 14:09:21 -0300

    :sparkles: Add Hook the Scroll

--- src/components/Model/ModelOverlay/index.tsx
+++ src/components/Model/ModelOverlay/index.tsx
@@ -1,6 +1,8 @@
+import useWrapperScroll from '../useWrapperScroll';
 import { Container } from './styles';
 
 const ModelOverlay: React.FC = ({children})=> {
+  const { scrollY } = useWrapperScroll()
   return (
     <Container>
      {children}
